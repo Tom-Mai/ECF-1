ebb33ec83153007d061898efdbb8848a56413e74
MAILLEUX <Mailleux.tom@gmail.com>
2022-11-09 14:27:10 +0100

    add js for tarifs/price; start array

---
+++ JS/price.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        id: 1,
+        category: "solo",
+        img: "/images/pricepic/solo.jpg",
+        title: "Juste moi",
+        description: "Séance pour une personne, en extérieur ou en studio.",
+        price: "130",
+    }
+]
+
+// TODO : faire le scirpt pour diff tarifs, comme le filtre portfolio.
