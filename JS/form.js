268ba5d49ab78b251184be1a34d4109fc36507c2
MAILLEUX <Mailleux.tom@gmail.com>
2022-11-22 15:17:09 +0100

    add option on contactPage, and a display when checked with .. a checkbox

--- JS/form.js
+++ JS/form.js
@@ -3,3 +3,17 @@ function setFocusToNameInput() {
     document.querySelector("#nameInput").focus();
 }
 setFocusToNameInput()
+
+
+function displayChecked() {
+    const checkbox = document.querySelector("#gridCheck1");
+    const selectInput =  document.querySelector("#inputPrice");
+     if(checkbox.checked === true) {
+        selectInput.style.display = "block";
+     }else {
+        selectInput.style.display = "none";
+     }
+     console.log(selectInput.innerHTML);
+}
+displayChecked();
+
