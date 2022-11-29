c06b48db589b4d0914cf169f30a566894646caf7
MAILLEUX <Mailleux.tom@gmail.com>
2022-11-29 08:23:25 +0100

    testins a few script for dynamic bc not working for now

--- JS/breadcrumb.js
+++ JS/breadcrumb.js
@@ -1,8 +1,19 @@
-document.getselectorAll("navbarNav a").addEventListener("click",(e) => {
-$select = $('<ol class="breadcrumb">');
-this.parent('li').each(function(n,li){
-    $select.prepend('/',li.children('a').cloneNode(true));
+// document.getselectorAll("navbarNav a").addEventListener("click",(e) => {
+// const select = $('<ol class="breadcrumb">');
+// this.parent('li').each(function(n,li){
+//  select.prepend('/',li.children('a').cloneNode(true));
+// });
+//     document.getElementsByClassName('display').html(
+//    select.prepend('<a href="#breadcrumb">Home</a>'));
+// })
+
+const getNav = document.querySelectorAll('#navbarNav a').addEventListener("click", function(){
+    let self = (this),
+        bc = document.querySelectorAll('<div class="collapse navbar-collapse text-center" id="navbarNav"></div>');
+
+        self.parent('li').each(function(n,li) {
+            let a = document.querySelectorAll('li').children('a').clone();
+            a.prepend(' / ', a);
 });
-    document.getElementsByClassName('display').html(
-        $select.prepend('<a href="#breadcrumb">Home</a>'));
+    document.querySelector('.breadcrumb').innerHTML.bc.prepend('<a href="#breadcrumb">Home</a>');
 })
