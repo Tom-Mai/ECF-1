ff48b60144d5f98b9e9900e427c9daec9771a639
Tom-Mai <88791693+Tom-Mai@users.noreply.github.com>
2023-01-03 06:03:02 +0100

    delete array after strignify it and write with fs(write) a json file. =>  Fetch it in the code

--- JS/price.js
+++ JS/price.js
@@ -1,55 +1,16 @@
-const data = [
-    {
-        id: 1,
-        category: "solo",
-        img: "/images/pricepic/solo.jpg",
-        alt : "photo of an old man",
-        title: "Juste moi",
-        description: "Séance pour une personne, en extérieur ou en studio.",
-        price: "130",
-    },
-    {
-        id: 2,
-        category: "famille",
-        img: "../images//family/family1.jpg",
-        alt : "family picture",
-        title: "Famille",
-        description: "Pour la famille ou les amis jusqu’à 4 personnes, en extérieur ou en studio 30 euros en supplément par personne au-delà de 4 (hormis enfant jusqu’à 2 ans).",
-        price: "250",
-    },
-    {
-        id: 3,
-        category: "Grossesse",
-        img: "../images/grossesse/grossesse1.jpg",
-        alt : "Pregnant picture",
-        title: "Il était une fois",
-        description: "Photo de grossesse (À votre domicile, en extérieur ou en studio).",
-        price: "500",
-    },
-    {
-        id: 4,
-        category: "bebe",
-        img: "../images/bebe/bebe1.jpg",
-        alt : "baby picture",
-        title: "Mon bébé",
-        description: "Photo d’enfant jusqu’à 3 ans (photo à domicile).",
-        price: "150",
-    },
-    {
-        id: 5,
-        category: "bapteme",
-        img: "../images/bapteme/bapteme1.jpg",
-        alt : "bapteme picture",
-        title: "J'immortalise l'événement",
-        description: "sur mesure Prestation de mariage ou baptême.",
-        price: "150",
-    },
-];
+
 const slideContainer = document.getElementById('slides-container');
 const prevButton = document.getElementById('slide-arrow-prev');
 const nextButton = document.getElementById('slide-arrow-next');
 onload = () => {
+    fetch('/priceCardData.json')
+    .then(response => response.json())
+    .then(data => {
+           // une fois que les données sont chargées, appelez la fonction displayData()
     displayData(data);
+})
+   .catch(err => console.log(err));
+
     const slide = document.querySelector('.slide');
     nextButton.addEventListener("click", ()=>{
         const slideWidth = slide.clientWidth;
